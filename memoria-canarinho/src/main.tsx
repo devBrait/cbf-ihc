403b1cbb84c0b4e983c8d73fc2b837f0f2b913bb
devBrait <lorimiervb@gmail.com>
2026-04-05 16:22:24 -0300

    Configs para deploy no GitHub Pages

--- memoria-canarinho/src/main.tsx
+++ memoria-canarinho/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App.tsx'
 import { CollectionProvider } from './context/CollectionContext.tsx'
 import { Toaster } from '@/components/ui/sonner'
@@ -8,11 +8,11 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <CollectionProvider>
         <App />
         <Toaster position="top-right" />
       </CollectionProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
